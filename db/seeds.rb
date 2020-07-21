35798b7a44af4cdf8310b88fe1f1920e453533fb
(Benja Kunkel kunkelbenjamin08@gmail.com) <kunkelbenjamin08@gmail.com>
2020-07-21 17:24:18 -0300

    benjsebpedia

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+# TODO: Write a seed to insert 100 posts in the database
+# db/seeds.rb
+puts 'Creating 10 fake articles...'
+10.times do
+  article = Article.new(
+    title: Faker::Lorem.sentences(number: 1, supplemental: true),
+    content: Faker::Lorem.paragraph_by_chars(number: 256, supplemental: false)
+  )
+  article.save!
+end
+puts 'Finished!'
